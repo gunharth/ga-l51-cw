c6f4fa39c981649a269e6cdc84e1214d8c4177f1
Gunharth Randolf <hello@gunharth.io>
2015-10-03 10:40:37 +0200

    Fixed error if format preis is 0

--- app/Format.php
+++ app/Format.php
@@ -21,31 +21,42 @@ class Format extends Model
     }
 
     public function calculateCosts() {
-        $brutto = $this->preis;
-        $rabatt = $this->preis;
-        $provision = $this->preis;
-        if($this->rabatt > 0) {
-            $rabatt = $rabatt - round(($this->preis/100)*$this->rabatt,2);
-            $provision = $rabatt;
-            $brutto = $rabatt;
-        }
-        if($this->provision > 0) {
-            $provision = $rabatt - round(($rabatt/100)*$this->provision,2);
-            $brutto = $provision;
-        }
-        $werbeabgabe = round($brutto*1.05,2);
-        //$werbeabgabe = number_format($werbeabgabe,2);
-        $ust = round($werbeabgabe*0.2,2);
+        if($this->type == 0) {
+            $brutto = $this->preis;
+            $rabatt = $this->preis;
+            $provision = $this->preis;
+            if($this->rabatt > 0) {
+                $rabatt = $rabatt - round(($this->preis/100)*$this->rabatt,2);
+                $provision = $rabatt;
+                $brutto = $rabatt;
+            }
+            if($this->provision > 0) {
+                $provision = $rabatt - round(($rabatt/100)*$this->provision,2);
+                $brutto = $provision;
+            }
+            $werbeabgabe = round($brutto*1.05,2);
+            //$werbeabgabe = number_format($werbeabgabe,2);
+            $ust = round($werbeabgabe*0.2,2);
 
-        $brutto = round($werbeabgabe*1.2);
-        return array(
-            'preis' => number_format($this->preis,2,'.',''), 
-            'rabatt' => number_format($rabatt,2,'.',''), 
-            'provision' => number_format($provision,2,'.',''), 
-            'werbeabgabe' => number_format($werbeabgabe,2,'.',''), 
-            'ust' => number_format($ust,2,'.',''), 
-            'brutto' => number_format($brutto,2,'.','')
-        );
+            $brutto = round($werbeabgabe*1.2);
+            return array(
+                'preis' => number_format($this->preis,2,'.',''), 
+                'rabatt' => number_format($rabatt,2,'.',''), 
+                'provision' => number_format($provision,2,'.',''), 
+                'werbeabgabe' => number_format($werbeabgabe,2,'.',''), 
+                'ust' => number_format($ust,2,'.',''), 
+                'brutto' => number_format($brutto,2,'.','')
+            );
+        } else {
+            return array(
+                'preis' => '0,00', 
+                'rabatt' => '0,00', 
+                'provision' => '0,00', 
+                'werbeabgabe' => '0,00', 
+                'ust' => '0,00', 
+                'brutto' => '0,00'
+            );
+        }
     }
 
     public function issue() {
