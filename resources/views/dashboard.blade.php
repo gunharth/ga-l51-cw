c594ba22c5e6def7ec1f8136caa90a8a402af595
Gunharth Randolf <hello@gunharth.io>
2015-11-07 13:02:40 +0100

    Added static ides to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,13 +6,42 @@
 	<div class="row vertical-align">
       <div class="col-sm-12 col-md-12">
       <h1>Dashboard</h1>
-        
         </div>
     </div>
     <hr />
-
-    <div class="well">
-        <p></p>
-        <p>&nbsp;</p>
+    
+    <div class="row">
+      <div class="col-sm-6">
+        <h2>Statistik Ideen </h2>
+        Umsatzzahlen (Gesamt/Verkäufer)<br>
+        Laufende offene Ausgaben (Direktlink zur Ineratenanlage)<br>
+        Status offener Ausgaben (Seitenauslastung, Umsatz)<br>
+        ...
+      </div>
+      <div class="col-sm-6">
+        <h2>Kommunikation</h2>
+        Notizen, Erinnerungen<br>
+        letzten 5 Verkäufe<br>
+        ...
+      </div>
+    </div>
+    <hr />
+    <h2>Chart Beispiele</h2>
+    <div class="row">
+      <div class="col-sm-6">
+        <div id="hero-graph" class="graph"></div>
+      </div>
+      <div class="col-sm-6">
+        <div id="hero-bar" class="graph"></div>
+      </div>
+    </div>
+    <hr />
+    <div class="row">
+      <div class="col-sm-6">
+        <div id="hero-area" class="graph"></div>
+      </div>
+      <div class="col-sm-6">
+        <div id="hero-donut" class="graph"></div>
+      </div>
     </div>
   @stop
