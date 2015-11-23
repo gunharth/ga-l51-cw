87218824b9f75627077386f3e649a2c9f2e82d84
Gunharth Randolf <hello@gunharth.io>
2015-11-23 12:11:09 +0100

    Update dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,35 +4,40 @@
 
 @section('content')
 	<div class="row vertical-align">
-      <div class="col-sm-12 col-md-12">
+    <div class="col-sm-12 col-md-12">
       <h1>Dashboard</h1>
-        </div>
     </div>
+  </div>
     <hr />
     <div class="row">
-    @foreach($issues as $issue)
-       <div class="col-sm-3 text-center">
-       <h3>{{ $issue->medium->title }} <br> {{ $issue->name }}</h3>
-       <div class="row">
-        <div class="col-sm-6">
-          <h4>Umsatz</h4> {{ $issue->totalNetto }} / {{ $issue->sollumsatz }}
-          <p class="data-attributes">
-  <span data-peity='{ "fill": ["green", "#eeeeee"],    "innerRadius": 10, "radius": 40 }'>{{ $issue->totalNettoRaw }}/{{ $issue->sollumsatzRaw }}</span>
-</p>
+      @foreach($issues as $issue)
+      <div class="col-sm-3 text-center">
+      <div style="border: 1px solid #e6e6e6; border-radius: 5px; padding: 5px;">
+        <h3>{{ $issue->medium->title }} <br> {{ $issue->name }}</h3>
+        <div class="row">
+          <div class="col-sm-6">
+            <h4>Umsatz</h4> <small>{{ $issue->totalNetto }} / {{ $issue->sollumsatz }}</small>
+            <p class="data-attributes">
+              <span data-peity='{ "fill": ["green", "#eeeeee"],    "innerRadius": 10, "radius": 40 }'>{{ $issue->totalNettoRaw }}/{{ $issue->sollumsatzRaw }}</span>
+            </p>
+            
+          </div>
+          <div class="col-sm-6">
+            <h4>Seiten</h4> <small>{{ $issue->totalFlaeche }} / {{ $issue->seiten }}</small>
+            <p class="data-attributes">
+              <span data-peity='{ "fill": ["green", "#eeeeee"],    "innerRadius": 10, "radius": 40 }'>{{ $issue->totalFlaeche }}/{{ $issue->seiten }}</span>
+            </p>
+          </div>
+          <div class="col-sm-12 text-right">
+        <a href="{{ route('medium.issues.show', [$issue->medium->slug,$issue->id]) }}" title="anzeigen"><i class="fa fa-lg fa-eye" data-toggle="tooltip" data-original-title="anzeigen"></i></a> 
         </div>
-        <div class="col-sm-6">
-          <h4>Seiten</h4> {{ $issue->totalFlaeche }} / {{ $issue->seiten }}
-          <p class="data-attributes">
-  <span data-peity='{ "fill": ["green", "#eeeeee"],    "innerRadius": 10, "radius": 40 }'>{{ $issue->totalFlaeche }}/{{ $issue->seiten }}</span>
-</p>
+        </div>
+        
         </div>
       </div>
-
-       
-
-       </div>
-    @endforeach
+      @endforeach
     </div>
+    <!--
     <div class="row">
       <div class="col-sm-6">
         <h2>Statistik Ideen </h2>
@@ -48,23 +53,5 @@
         ...
       </div>
     </div>
-    <hr />
-    <h2>Chart Beispiele</h2>
-    <div class="row">
-      <div class="col-sm-6">
-        <div id="hero-graph" class="graph"></div>
-      </div>
-      <div class="col-sm-6">
-        <div id="hero-bar" class="graph"></div>
-      </div>
-    </div>
-    <hr />
-    <div class="row">
-      <div class="col-sm-6">
-        <div id="hero-area" class="graph"></div>
-      </div>
-      <div class="col-sm-6">
-        <div id="hero-donut" class="graph"></div>
-      </div>
-    </div>
+    -->
   @stop
