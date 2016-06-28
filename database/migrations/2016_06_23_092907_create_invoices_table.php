d4d8c3cc6971c2a96c0e644b8f7613f2d7cb1335
Gunharth Randolf <hello@gunharth.io>
2016-06-28 11:17:31 +0200

    Fix migration typo

--- database/migrations/2016_06_23_092907_create_invoices_table.php
+++ database/migrations/2016_06_23_092907_create_invoices_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateInvoisesTable extends Migration
+class CreateInvoicesTable extends Migration
 {
     /**
      * Run the migrations.
